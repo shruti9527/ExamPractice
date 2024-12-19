4fdbe8807c13ffa354ee4cafad3b68273bfae3c9
shruti9527 <shrutideshpande2020.it@mmcoe.edu.in>
2024-12-19 23:57:04 +0530

    first commit

---
+++ src/PracticeWithMe/LetsGo.java
@@ -0,0 +1,10 @@
+package PracticeWithMe;
+
+public class LetsGo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It is what it is");
+	}
+
+}
